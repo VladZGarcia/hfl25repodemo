e49e5edcfe1c02e15d3ceea4ff43149404f5caac
VladZGarcia <113976475+VladZGarcia@users.noreply.github.com>
2025-02-05 10:51:22 +0100

    fix main and yaml
    fix main and yaml

--- assignment_1/bin/assignment_1.dart
+++ assignment_1/bin/assignment_1.dart
@@ -1,40 +1,5 @@
-import 'dart:io';
-
-import 'package:assignment_1/repositories/person_repository.dart';
-import 'package:assignment_1/repositories/vehicle_repository.dart';
+import 'package:assignment_1/cli.dart';
 
 void main() {
-  var personRepo = PersonRepository();
-  var vehicleRepo = VehicleRepository();
-
-  while (true) {
-    print('Wellcome to the Parking App!');
-    print('Select an option:');
-    print('1. Handle Persons?');
-    print('2. Handle Vehicles?');
-    print('5. Quit.');
-    stdout.write('Choose alternativ (1-5): ');
-    var choice = stdin.readLineSync();
-
-    switch (choice) {
-      case '1':
-        handlePersons(personRepo);
-        break;
-      case '2':
-        handleVehicles(vehicleRepo, personRepo);
-        break;
-      case '5':
-        exit(0);
-      default:
-        print('Not valid, try again.');
-    }
-  }
-}
-
-void handlePersons(PersonRepository repo) {
-  //Todo
-}
-
-void handleVehicles(VehicleRepository repo, PersonRepository personRepo) {
-  //Todo
+  runCLI();
 }
